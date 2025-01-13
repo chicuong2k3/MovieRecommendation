955f33b1b49f1f19813e5ef04cd12c18aba0fead
21120408 <dangtuananh3012003@gmail.com>
2025-01-13 23:48:01 +0700

    thêm authen firebase

--- MovieRecommendationApi/ClaimsPrincipalExtensions.cs
+++ MovieRecommendationApi/ClaimsPrincipalExtensions.cs
@@ -2,13 +2,14 @@
 
 namespace MovieRecommendationApi
 {
-    public static class ClaimsPrincipalExtensions
-    {
-        public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
-        {
-            return new Guid("a857cbb5-996e-49d5-ae7d-20d943f2ebcd").ToString();
-            //return claimsPrincipal.FindFirstValue("sub")
-            //    ?? throw new InvalidOperationException("Invalid User Id");
-        }
-    }
+	public static class ClaimsPrincipalExtensions
+	{
+		public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+		{
+			// Lấy giá trị của Claim chứa UID người dùng
+			return claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier)
+				   ?? throw new InvalidOperationException("User ID not found in claims.");
+		}
+	}
 }
+
---
+++ MovieRecommendationApi/Controllers/AuthenFirebase/FirebaseTokenVerifier.cs
@@ -0,0 +1,36 @@
+﻿
+using FirebaseAdmin;
+using FirebaseAdmin.Auth;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text.Json;
+
+namespace MovieRecommendationApi.Controllers.AuthenFirebase
+{
+	public class FirebaseTokenVerifier
+	{
+		public FirebaseTokenVerifier()
+		{
+			// Khởi tạo Firebase Admin SDK
+			FirebaseApp.Create(new AppOptions
+			{
+				Credential = GoogleCredential.FromFile("final-awad-firebase-adminsdk-gyhou-f59ac6756c.json")
+			});
+		}
+
+		public async Task<FirebaseToken> VerifyIdTokenAsync(string idToken)
+		{
+			try
+			{
+				FirebaseToken decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(idToken);
+				return decodedToken; // Trả về thông tin user nếu token hợp lệ
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Token verification failed: {ex.Message}");
+				throw new UnauthorizedAccessException("Invalid ID token.");
+			}
+		}
+	}
+}
