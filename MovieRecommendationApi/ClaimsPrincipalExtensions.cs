a5247caee86b4ffa7300f671d7486f9f6bff418c
chicuong2k3 <Acer@DESKTOP-DE1DEP4>
2025-01-13 10:37:08 +0700

    add movie rating and adding movie to watchlist

---
+++ MovieRecommendationApi/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace MovieRecommendationApi
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal claimsPrincipal)
+        {
+            return new Guid("a857cbb5-996e-49d5-ae7d-20d943f2ebcd").ToString();
+            //return claimsPrincipal.FindFirstValue("sub")
+            //    ?? throw new InvalidOperationException("Invalid User Id");
+        }
+    }
+}
